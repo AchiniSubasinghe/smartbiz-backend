fd62e0c03c09e4cc42e902d67037b7ee8ecce10d
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-11-26 02:21:52 +0530

    feat(product): add Product service layer with CRUD operations

--- src/main/java/com/smartbiz/service/ProductService.java
+++ src/main/java/com/smartbiz/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.smartbiz.service;
 
+import com.smartbiz.entity.Product;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ProductService {
+    Product addProduct(Product product);
+    Product updateProduct(UUID id,Product product);
+    void deleteProduct(UUID id);
+    Product getProductById(UUID id);
+    Product getProductBySku(String sku);
+    List<Product> getAllProducts();
 }
+
+
--- src/main/java/com/smartbiz/service/impl/ProductServiceImpl.java
+++ src/main/java/com/smartbiz/service/impl/ProductServiceImpl.java
@@ -1,4 +1,68 @@
 package com.smartbiz.service.impl;
 
-public class ProductServiceImpl {
+import com.smartbiz.entity.Product;
+import com.smartbiz.repo.ProductRepo;
+import com.smartbiz.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepo productRepo;
+
+    public ProductServiceImpl(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    // Create Product
+    @Override
+    public Product addProduct(Product product) {
+        if (productRepo.existsBySku(product.getSku())) {
+            throw new RuntimeException("SKU already exists");
+        }
+        return productRepo.save(product);
+    }
+
+    // Update Product
+    @Override
+    public Product updateProduct(UUID id, Product updatedDetails) {
+        Product product = getProductById(id);
+
+        product.setSku(updatedDetails.getSku());
+        product.setName(updatedDetails.getName());
+        product.setUnitPrice(updatedDetails.getUnitPrice());
+        product.setQuantity(updatedDetails.getQuantity());
+
+        return productRepo.save(product);
+    }
+
+    // Delete Product
+    @Override
+    public void deleteProduct(UUID id) {
+        Product product = getProductById(id);
+        productRepo.delete(product);
+    }
+
+    // Get by ID
+    @Override
+    public Product getProductById(UUID id) {
+        return productRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    // Get by SKU
+    @Override
+    public Product getProductBySku(String sku) {
+        return productRepo.findBySku(sku)
+                .orElseThrow(() -> new RuntimeException("Product not found by SKU"));
+    }
+
+    // Get all products
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
 }
