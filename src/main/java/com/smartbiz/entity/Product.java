f146ee2b7474306c1804038927d6c426d16f9a88
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-11-26 00:34:03 +0530

    feat: add Product entity

--- src/main/java/com/smartbiz/entity/Product.java
+++ src/main/java/com/smartbiz/entity/Product.java
@@ -1,4 +1,38 @@
 package com.smartbiz.entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+//    stock keeping unit
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double quantity;
+
+    @Column (nullable = false)
+    private double unitPrice;
+
+    @Column(nullable = false ,updatable = false )
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
 }
