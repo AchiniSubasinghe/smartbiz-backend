bec74eb17e1c65cace6e4f1e26f0ecf110b68e31
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-01 08:43:39 +0530

    feat:add the sale service

--- src/main/java/com/smartbiz/service/SaleService.java
+++ src/main/java/com/smartbiz/service/SaleService.java
@@ -1,4 +1,16 @@
 package com.smartbiz.service;
 
+import com.smartbiz.dto.request.SaleRequestDto;
+import com.smartbiz.dto.response.SaleResponseDto;
+import com.smartbiz.entity.Sale;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 public interface SaleService {
+    SaleResponseDto createSale(SaleRequestDto dto);
+    List<Sale> getAllSales();
+    Sale getSaleById(UUID id);
+    List<Sale> getSalesBetween(LocalDateTime start, LocalDateTime end);
 }
