0e4fe734f12aa583deab4e9e88868345ec0f7483
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-01 08:38:52 +0530

    feat:add sale entity

--- src/main/java/com/smartbiz/entity/Sale.java
+++ src/main/java/com/smartbiz/entity/Sale.java
@@ -1,5 +1,42 @@
 package com.smartbiz.entity;
 
-public class Sale {
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
+public class Sale {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false,unique = true)
+    private String invoiceNumber;
+
+    @Column(nullable = false)
+    private String productSku;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private double quantity;
+
+    @Column(nullable = false)
+    private double unitPrice;
+
+    @Column(nullable = false)
+    private double totalAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
