81ee254c65aac63b15e2dc3efbf35f85d6332789
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-01 08:40:17 +0530

    feat:add sale repository

--- src/main/java/com/smartbiz/repo/SaleRepo.java
+++ src/main/java/com/smartbiz/repo/SaleRepo.java
@@ -1,4 +1,15 @@
 package com.smartbiz.repo;
 
-public interface SaleRepo {
+import com.smartbiz.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface SaleRepo extends JpaRepository<Sale, UUID> {
+    List<Sale> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+    List<Sale> findByProductSku(String sku);
+
 }
