152801af93db09e269496d35b2c295799a7c0234
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-01 08:41:48 +0530

    feat:add sale response dto

--- src/main/java/com/smartbiz/dto/response/SaleResponseDto.java
+++ src/main/java/com/smartbiz/dto/response/SaleResponseDto.java
@@ -1,4 +1,32 @@
 package com.smartbiz.dto.response;
 
+import com.smartbiz.entity.Sale;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter @Builder
 public class SaleResponseDto {
+    private UUID id;
+    private String invoiceNumber;
+    private String productSku;
+    private String productName;
+    private double quantity;
+    private double unitPrice;
+    private double totalAmount;
+    private String createdAt;
+
+    public static SaleResponseDto fromEntity(Sale sale) {
+        return SaleResponseDto.builder()
+                .id(sale.getId())
+                .invoiceNumber(sale.getInvoiceNumber())
+                .productSku(sale.getProductSku())
+                .productName(sale.getProductName())
+                .quantity(sale.getQuantity())
+                .unitPrice(sale.getUnitPrice())
+                .totalAmount(sale.getTotalAmount())
+                .createdAt(sale.getCreatedAt().toString())
+                .build();
+    }
 }
