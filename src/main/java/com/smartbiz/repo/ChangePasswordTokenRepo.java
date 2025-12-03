6ecc993464fed8944d5cc4e828fb8c9b59044ded
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-03 12:55:16 +0530

    feat(repo): add PasswordResetTokenRepo for password reset token management

---
+++ src/main/java/com/smartbiz/repo/ChangePasswordTokenRepo.java
@@ -0,0 +1,12 @@
+package com.smartbiz.repo;
+
+import com.smartbiz.entity.ChangePasswordToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ChangePasswordTokenRepo extends JpaRepository<ChangePasswordToken, UUID>{
+        Optional<ChangePasswordToken> findByToken(String token);
+        void deleteByToken(String token);
+}
