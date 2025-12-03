4a034b54463571d24134ba317f8b7b412132e32f
AchiniSubasinghe <achinimaheshika12@gmail.com>
2025-12-03 21:52:04 +0530

    feat: update AuthController with password reset and email delivery endpoints

--- src/main/java/com/smartbiz/controller/AuthController.java
+++ src/main/java/com/smartbiz/controller/AuthController.java
@@ -1,9 +1,10 @@
 package com.smartbiz.controller;
 
-import com.smartbiz.dto.request.ChangePasswordDto;
+import com.smartbiz.dto.request.RestEmailRequestDto;
 import com.smartbiz.dto.request.SignInRequestDto;
 import com.smartbiz.dto.request.SignUpRequestDto;
 import com.smartbiz.dto.response.ApiResponseDto;
+import com.smartbiz.dto.response.RestPasswordConfirmDto;
 import com.smartbiz.dto.response.SignInResultDto;
 import com.smartbiz.dto.response.UserResponseDto;
 import com.smartbiz.service.AuthService;
@@ -12,74 +13,85 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//handle the api request and return json response
+// handle the api request and return json response
 @RestController
-
-//use the root directly POST PUT GET or DELETE
+// use the root directly POST PUT GET or DELETE
 @RequestMapping()
-
-//allow sending cookies from back end to front end(putting bcz back end and frontend
+// allow sending cookies from back end to front end(putting bcz back end and frontend
 // running in different ports)
 @CrossOrigin(
         origins = "http://localhost:3000",
         allowCredentials = "true"
 )
-
 public class AuthController {
-    //dependancy injection
+
+    // dependency injection
     private final AuthService authService;
 
-    //constructor injection
+    // constructor injection
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
-//Regsiter
+    // Register
     @PostMapping("/signUp")
-//    frontend send http post request as json body
+    // frontend send http post request as json body
     public ResponseEntity<ApiResponseDto<UserResponseDto>>
-//    @Request converts json into signup request dto
-//    @Valid checks validation rules
+    // @Request converts json into signup request dto
+    // @Valid checks validation rules
     signUp(@RequestBody @Valid SignUpRequestDto dto) {
-//  Controller sends DTO to AuthService
+
+        // Controller sends DTO to AuthService
         UserResponseDto response = authService.addUser(dto);
         return ResponseEntity.ok(new ApiResponseDto<>(true, "User registered successfully", response));
     }
 
-//LogIN
-@PostMapping("/signIn")
-public ResponseEntity<ApiResponseDto<SignInResultDto>> signIn(@RequestBody @Valid SignInRequestDto dto) {
-    SignInResultDto response = authService.signIn(dto);
-    ApiResponseDto<SignInResultDto> body = new ApiResponseDto<>(true, "Login successful", response);
-    if (response.getCookie() != null) {
-        // include Set-Cookie header so browser receives httpOnly cookie
-        return ResponseEntity.ok()
-                .header("Set-Cookie", response.getCookie().toString())
-                .body(body);
-    } else {
-        return ResponseEntity.ok(body);
-    }
-}
+    // Login
+    @PostMapping("/signIn")
+    public ResponseEntity<ApiResponseDto<SignInResultDto>> signIn(@RequestBody @Valid SignInRequestDto dto) {
+        SignInResultDto response = authService.signIn(dto);
+        ApiResponseDto<SignInResultDto> body = new ApiResponseDto<>(true, "Login successful", response);
 
-    //change password
-    @PostMapping("/change-password")
-    public ResponseEntity<ApiResponseDto<Object>> changePassword(@RequestBody @Valid ChangePasswordDto dto) {
-        // confirm password
-        if (!dto.getNewPassword().equals(dto.getConfirmPassword())) {
-            return ResponseEntity.badRequest().body(
-                    new ApiResponseDto<>(false, "Passwords do not match", null)
-            );
+        if (response.getCookie() != null) {
+            // include Set-Cookie header so browser receives httpOnly cookie
+            return ResponseEntity.ok()
+                    .header("Set-Cookie", response.getCookie().toString())
+                    .body(body);
+        } else {
+            return ResponseEntity.ok(body);
+        }
+    }
+
+    // send reset email
+    @PostMapping("/send-reset-email")
+    public ResponseEntity<ApiResponseDto<Object>> sendRestEmail(@RequestBody RestEmailRequestDto restEmailRequestDto) {
+        authService.sendResetEmail(restEmailRequestDto.getEmail());
+        return ResponseEntity.ok(new ApiResponseDto<>(true, "Reset link send to your email", null));
+    }
+
+    // reset password confirm
+    @PostMapping("/reset-password-confirm")
+    public ResponseEntity<ApiResponseDto<Object>> resetPassword(@RequestBody @Valid RestPasswordConfirmDto restPasswordConfirmDto) {
+
+        if (!restPasswordConfirmDto.getNewPassword().equals(restPasswordConfirmDto.getConfirmPassword())) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponseDto<>(false, "Passwords do not match", null));
         }
 
-        UserResponseDto updated = authService.changePassword(dto.getUsername(), dto.getNewPassword());
-        //ApiResponseDto<Object> response = new ApiResponseDto<>(true, "Password changed successfully", updated);
-        return ResponseEntity.ok(new ApiResponseDto<>(
-                true,"Password reset successfully",updated
-        ));
+        try {
+            authService.resetPasswordWithToken(restPasswordConfirmDto.getToken(), restPasswordConfirmDto.getNewPassword());
+
+            return ResponseEntity.ok(
+                    new ApiResponseDto<>(true, "Password reset successfully", null));
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(
+                    new ApiResponseDto<>(false, e.getMessage(), null)
+            );
+        }
     }
 
-
-    //Log Out
+    // Log Out
     @PostMapping("/logout")
     public ResponseEntity<ApiResponseDto<Object>> logout() {
         ResponseCookie cookie = ResponseCookie.from("jwt", "")
