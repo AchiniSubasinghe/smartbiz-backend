8bc79f4288485372319f159c46d7cb49f3f13610
Achini Subasinghe <achinimaheshika12@gmail.com>
2025-12-26 18:49:14 +0530

    refactor: decouple SubscriptionPlan service from entity using DTOs

--- src/main/java/com/smartbiz/service/SubscriptionPlanService.java
+++ src/main/java/com/smartbiz/service/SubscriptionPlanService.java
@@ -1,15 +1,17 @@
-package com.smartbiz.service;
+    package com.smartbiz.service;
 
-import com.smartbiz.entity.SubscriptionPlan;
+    import com.smartbiz.dto.request.SubscriptionPlanRequestDto;
+    import com.smartbiz.dto.response.SubscriptionPlanResponseDto;
+    import com.smartbiz.entity.SubscriptionPlan;
 
-import java.util.List;
-import java.util.UUID;
+    import java.util.List;
+    import java.util.UUID;
 
-public interface SubscriptionPlanService {
-    SubscriptionPlan create(SubscriptionPlan plan);
-    List<SubscriptionPlan> getAll();
-    SubscriptionPlan getById(UUID id);
-    SubscriptionPlan getByName(String name);
-    SubscriptionPlan update(UUID id, SubscriptionPlan plan);
-    void delete(UUID id);
-}
+    public interface SubscriptionPlanService {
+        SubscriptionPlanResponseDto create(SubscriptionPlanRequestDto plan);
+        List<SubscriptionPlanResponseDto> getAll();
+        SubscriptionPlanResponseDto getById(UUID id);
+        SubscriptionPlanResponseDto getByName(String name);
+        SubscriptionPlanResponseDto update(UUID id, SubscriptionPlanRequestDto plan);
+        void delete(UUID id);
+    }
